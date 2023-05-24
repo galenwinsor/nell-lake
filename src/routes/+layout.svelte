a7bf5368b5ff01cc3b44d9a6eb44be0892c3a6b3
galenwinsor <galen_winsor@alumni.brown.edu>
2023-05-24 13:53:41 -0400

    feat: navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import "../global.css";
+	import NL from "$lib/assets/NL.svg?raw";
+</script>
+
+<div class="background" />
+<div class="container">
+	<nav>
+		<a href="/">
+			{@html NL}
+		</a>
+		<hr />
+		<a href="/books">BOOKS</a>
+		<a href="/books">ARTICLES</a>
+		<a href="/books">BIO</a>
+	</nav>
+	<slot />
+</div>
+
+<style>
+	.background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		z-index: -20;
+
+		background: var(--red-0);
+	}
+
+	nav {
+		display: flex;
+		gap: 2rem;
+		align-items: center;
+
+		padding: 4rem 0;
+	}
+
+	nav hr {
+		color: var(--blue-6);
+
+		width: 1.5rem;
+
+		transform: rotate(90deg);
+	}
+
+	nav a {
+		color: var(--blue-6);
+	}
+
+	.container {
+		margin: 0 clamp(4rem, 10%, 20rem);
+	}
+</style>
